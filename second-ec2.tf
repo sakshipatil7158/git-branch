332e6fcddce08497fb08f1060bd689eb57a798b6
sakshi <patilsakshi7158@gmail.com>
2025-05-09 16:33:42 +0530

    third change

--- second-ec2.tf
+++ second-ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform" {
   ami           = "ami-0c1ac8a41498c1a9c"
   instance_type = "t3.micro"
   key_name = "newins"
-  vpc_security_group_ids = "sg-05bc084a89364092e"
+  vpc_security_group_ids = "launch-wizard-8"
 
   tags = {
     Name = "HelloWorld"
