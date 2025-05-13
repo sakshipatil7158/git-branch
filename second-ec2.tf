99a3c3fb615ce153e99430642fb81c13cd412f1c
sakshi <patilsakshi7158@gmail.com>
2025-05-13 07:07:29 +0530

    terraform file added in s3 bucket

--- second-ec2.tf
+++ second-ec2.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-7158"
+    region = "eu-north-1"
+    key = "terraform.tfstate"
+    
+  }
+}
+
 resource "aws_instance" "terraform" {
   ami           = "ami-0c1ac8a41498c1a9c"
   instance_type = "t3.micro"
